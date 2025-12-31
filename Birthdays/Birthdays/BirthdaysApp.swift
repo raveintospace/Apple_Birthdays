3f12676434919f5b35c4719c63a0fe15df46db76
Oriol Angelet <uriaeri@gmail.com>
2025-12-31 11:49:29 +0100

    Initial commit

---
+++ Birthdays/Birthdays/BirthdaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirthdaysApp.swift
+//  Birthdays
+//
+//  Created by Uri on 31/12/25.
+//
+
+import SwiftUI
+
+@main
+struct BirthdaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
